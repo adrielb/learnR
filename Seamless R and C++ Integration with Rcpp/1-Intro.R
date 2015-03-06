296d4fdd9a6fb428bd373d046970e215209bba7a
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-03-05 22:00:40 -0500

    Rcpp: memoization

--- Seamless R and C++ Integration with Rcpp/1-Intro.R
+++ Seamless R and C++ Integration with Rcpp/1-Intro.R
@@ -1,3 +1,5 @@
+library(inline)
+
 fibR <- function(n) {
 	if (n==0) return(0)
 	if (n==1) return(1)
@@ -6,7 +8,6 @@ fibR <- function(n) {
 
 fibR( 10 )
 
-library(inline)
 
 incltxt <- '
 int fibonacci( const int x ) {
@@ -26,5 +27,67 @@ fibRcpp <- cxxfunction(signature(xs="int"),
 ')
 
 fibRcpp( 10 )
+
+
+# using attributes
+sourceCpp( "fibonacci.cpp" )
+fibonacci( 20 )
+
+# memoization in R
+mfibR <- local({
+	memo <- c(1,1,rep(NA,1000))
+	f <- function(x) {
+		if (x==0) return(0)
+		if (x <0) return(NA)
+		if (x>length(memo)) stop("x too big")
+		if (!is.na(memo[x])) return( memo[x] )
+		ans <- f(x-2) + f(x-1)
+		memo[x] <- ans
+		ans
+	}
+})
+
+mfibR( 10 )
+
+# memoization in C++
+mincltxt <- '
+#include <algorithm>
+#include <stdexcept>
+#include <cmath>
+#include <iostream>
+class Fib {
+public:
+	Fib( unsigned int n = 1000 ) {
+		memo.resize(n);
+		std::fill( memo.begin(), memo.end(), NAN);
+		memo[0] = 1.0;
+		memo[1] = 1.0;
+	}
+	double fibonacci( int x ) {
+		if( x < 0 ) {
+			return( (double) NAN);
+		}
+		if( x >= (int) memo.size() ) 
+			throw std::range_error(\"x too large\");
+		if (! ::isnan( memo[x] ) )
+			return memo[x];
+		memo[x] = fibonacci(x-2) + fibonacci(x-1);
+		return memo[x];
+	}
+private:
+	std::vector<double> memo;
+};
+'
+
+mfibRcpp <- cxxfunction(signature(xs="int"),
+												plugin="Rcpp",
+												includes=mincltxt,
+												body='
+int x = Rcpp::as<int>(xs);
+Fib f;
+return Rcpp::wrap( f.fibonacci(x-1) );
+')
+
+mfibRcpp(10)
 
 
