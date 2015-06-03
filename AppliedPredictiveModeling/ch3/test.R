836086d984b130b18755a8e20b784a84d097cb3f
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-06-03 19:17:06 -0400

    APM: 3 transformations

---
+++ AppliedPredictiveModeling/ch3/test.R
@@ -0,0 +1,101 @@
+library(ggplot2)
+library(dplyr)
+
+# Box Cox to resolve skewness# {{{
+BoxCox <- function ( x, l ) {
+  (x^l-1) / l
+}
+
+x <- seq(-5,5,length.out=100)  
+bc <- BoxCox( x, 2 )
+plot( x, bc, ylim=c(-5,5) )
+lines( x, BoxCox( x, -1 ))
+lines( x, BoxCox( x, 0.5 ))
+# }}}
+
+# spatial sign transform # {{{
+n <- 1000
+df <- data.frame( a=rnorm(n), b=rnorm(n) )
+
+df$s <- sqrt(df$a^2 + df$b^2)
+df$a2 <- df$a / df$s
+df$b2 <- df$b / df$s
+
+ggplot( data=df, aes(x=a, y=b)) +
+  geom_point() +
+  ggtitle( "original data" )
+
+ggplot( data=df, aes(x=a2, y=b2)) +
+  geom_point() +
+  ggtitle( "spatial sign transform" )
+
+qplot( x=seq_along(df$s), y=s, data=df, geom="point")
+# }}}
+
+apropos('geom')
+
+?geom_point
+
+RSiteSearch( 'geom_point'  )
+
+install.packages( "AppliedPredictiveModeling" )
+
+library(AppliedPredictiveModeling)
+
+data(segmentationOriginal)
+
+typeof(segmentationOriginal)
+
+names(segmentationOriginal)
+
+nrow( segmentationOriginal)
+
+segData <- segmentationOriginal %>% subset( Case == "Train" )
+
+cellID <- segData$Cell
+class <- segData$Class
+case <- segData$Case
+segData <- segData[,-(1:3)]
+
+# columns with binary data
+statusColNum <- grep( "Status", names(segData) )
+
+# remove those cols
+segData <- segData[, -statusColNum]
+
+names(segmentationOriginal)
+
+#
+# Transformations
+install.packages( "e1071" )
+
+library(e1071)
+
+skewness(segData$AngleCh1)
+
+skewValues <- apply( segData, 2, skewness )
+
+head( skewValues )
+
+hist( skewValues )
+
+qplot( x=skewValues, geom="bar")
+
+df <- data.frame( skewValues )
+df$v=names(skewValues)
+df <- df %>% arrange( skewValues )
+head(df)
+
+qplot( x=v, y=skewValues, data=df, geom='bar', stat='identity')
+
+install.packages( "caret" )
+
+library(caret)
+
+Ch1AreaTrans <- BoxCoxTrans( segData$AreaCh1 )
+
+head( segData$AreaCh1 )
+
+predict( Ch1AreaTrans, head( segData$AreaCh1 ))
+
+
